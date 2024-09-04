beb0c5964d26dcff983531c3dbc1062fa0c4d5fc
Vincent Zhang <84146716+vincentzhang15@users.noreply.github.com>
2024-09-04 18:53:14 -0400

    LangChain Ollama

--- 2024-09-04_LangChainOllama/langchainollama.py
+++ 2024-09-04_LangChainOllama/langchainollama.py
@@ -19,22 +19,25 @@ https://github.com/ollama/ollama-python
 
 import asyncio
 
-async def langchainollama():
+MODEL = "llama3.1:405b"
+# MODEL = "qwen2:72b-instruct"
+
+async def langchain_ollama():
     from langchain_ollama.llms import OllamaLLM
     from langchain_core.prompts import ChatPromptTemplate
     from langchain_core.output_parsers import StrOutputParser
 
-    model = OllamaLLM(model="qwen2:72b-instruct")
+    model = OllamaLLM(model=MODEL)
     prompt = ChatPromptTemplate.from_template("How to make a(n) {item}?")
     parser = StrOutputParser()
     chain = prompt | model | parser
 
     async for chunk in chain.astream({"item": "apple pie"}):
-        print(chunk, flush=True)
+        print(chunk, end="", flush=True)
 
-def ollama_only():
+def ollama_python():
     import ollama
-    model = "qwen2:72b-instruct"
+    model = MODEL
     item = "apple pie"
     messages = [{"role": "user", "content": f"How to make a(n) {item}?"}]
     stream = ollama.chat(model=model, messages=messages, stream=True)
@@ -42,5 +45,8 @@ def ollama_only():
         print(chunk['message']['content'], end="", flush=True)
 
 if __name__ == "__main__":
-    asyncio.run(langchainollama())
-    ollama_only()
+    print(" ========================== LangChain + Ollama  ========================== ")
+    asyncio.run(langchain_ollama())
+    print("\n\n ========================== Ollama Python  ========================== ")
+    ollama_python()
+    print("\n")
